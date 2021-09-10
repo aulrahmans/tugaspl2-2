2e283cb4f0658f45b882a1c5783eff5263a71419
aulrahmans <89183295+aulrahmans@users.noreply.github.com>
2021-09-10 22:31:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,45 @@
-<table width="100%" height="100%" border="1">
-    <tr height="5%">
-        <td align="center">
-            <a href="index.php?id=<?php echo base64_encode(1);?>">Mahasiswa</a>
-            <a href="index.php?id=2">Dosen</a>
-            <a href="index.php?id=3">Matakuliah</a>
-            <a href="index.php?id=4">Nilai</a>
-        </td>
-    </tr>
-    <tr height="95%">
-        <td valign="top" align="center">
-            <?php
-                $id_menu =base64_decode($_GET["id"]);
-                if($id_menu==1)
-                {
-                    include('latihan1.php');
-                }
-                elseif($_GET["id"]==2)
-                {
-                    echo "belum ada form";
-                }
-                elseif($_GET["id"]==3)
-                {
-                    echo "belum ada form";
-                }
-                elseif($_GET["id"]==4)
-                {
-                    echo "belum ada form";
-                }
-            ?>
-        </td>
-    </tr>
+<?php
+function url ($id)
+{
+    return "index.php?id=" . base64_encode($id);
+}
+
+function content()
+{
+$id_menu = base64_decode(@$_GET['id']);
+switch ($id_menu) {
+    case 1:
+        include ('latihan1.php');
+        break;
+
+    case 2:
+            include ('latihan2.php');
+        break;
+    case 3:
+        echo 'Matakuliah';
+        break;
+    case 4:
+        echo 'Nilai';
+        break;
+    default:
+        echo 'Form belum ada..';
+        break;
+    }
+}
+?>
+
+<table width="100%" height="100%" border="1">
+    <tr height="5%">
+        <td align ="center">
+            <a href="<?=url(1); ?>">Mahasiswa </a>
+            <a href="<?=url(3); ?>">Dosen </a>
+            <a href="<?=url(3); ?>">Mata kuliah </a>
+            <a href="<?=url(4); ?>">Nilai </a>
+        </td>
+    </tr>
+    <tr height="95%">
+        <td valign="top" align="center">
+           <?=content(); ?>;
+        </td>
+    </tr>
 </table>
